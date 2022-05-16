19466e8e3d3bead5cd81c1665601502df290116c
Lamech Israel <lamech639@revature.net>
2022-05-16 13:29:47 -0700

    Update Login Page

--- src/components/user-login-page.js
+++ src/components/user-login-page.js
@@ -1,39 +1,56 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 
-export default function UserLogInPage(){
+export default function UserLogInPage() {
 
     //Dummy values
     const [username, setUserName] = useState("")
     const [password, setPassword] = useState("")
+    const navigate = useNavigate()
 
     //Update local values
-    function updateUserName(event){
+    function updateUserName(event) {
         setUserName(event.target.value)
     }
 
     //update local value
-    function updatePassword(event){
+    function updatePassword(event) {
         setPassword(event.target.value)
     }
 
+    async function sendLoginInfo() {
+        const loginInfo = { username, password }
+        const response = await fetch(`http://potlukk-env.eba-cnm6zrpt.us-east-2.elasticbeanstalk.com/potlucks/login`, {
+            method: "DELETE",
+            body: JSON.stringify(loginInfo),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        const userInfo = await response.json()
+        sessionStorage.setItem("user", JSON.stringify(userInfo));
+        navigate("/potlucks")
+    }
+
     //jsx "html" response
-    return(<>
+    return (<>
 
         <h1>User Log in</h1>
 
         <fieldset>
             <label htmlFor="Username">UserName</label>
-            <br/>
-            <input onChange={updateUserName} name="username" type= "text" placeholder="JaneDoe"/>
-            <br/>
+            <br />
+            <input onChange={updateUserName} name="username" type="text" placeholder="JaneDoe" />
+            <br />
             <label htmlFor="password">Password</label>
-            <br/>
-            <input onChange={updatePassword} name="password" type = "password"/>
-            <br/>
-            <button>Log In</button>    
+            <br />
+            <input onChange={updatePassword} name="password" type="password" />
+            <br />
+            <button onClick={sendLoginInfo}>Log In</button>
         </fieldset>
-        
+
     </>)
 
 }
